00f719903376fc8c0a5ec657e7233ef6e63bda20
anthony <stackchain@gmail.com>
2019-04-10 13:46:56 -0400

    animation stretch goal done

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,4 +1,10 @@
 
+
+TweenMax.from(".header", 1.5, {opacity:0, scale:0, ease:Bounce.easeOut});
+TweenMax.staggerFrom(".article", 0.5, {opacity:0, y:200, rotatition:180, scale:2, delay:0.5}, 0.2)
+
+
+
 const toggleMenu = () => {
   menu.classList.toggle('menu--open');
   // Toggle the "menu--open" class on your menu refence. 
